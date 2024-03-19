08ba531d4b63514b52dac11417b76cf91c4eadd6
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-19 01:19:27 -0300

    Added the function to update the password-length value

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let sliderElement     = document.querySelector("#slider");              // Acess
 let buttonElement     = document.querySelector("#button");              // Acesso ao elemento button dentro do HTML
 
 let sizePassword      = document.querySelector("#valor");               // Acesso ao elemento valor dentro do HTML
-let password          = document.querySelector("#password");            // Acessp ao elemento password dentro do HTML
+let password          = document.querySelector("#password");            // Acesso ao elemento password dentro do HTML
 let containerPassword = document.querySelector("#container-password");  // Acesso ao elemento container-password dentro do HTML
 
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&" // Todos os caracteres que podem ser usados ao gerar uma senha
+let novaSenha = "";
 
+sizePassword.innerHTML = sliderElement.value;
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
 
+    
