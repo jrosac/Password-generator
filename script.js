5077ded672e308f5ba5720c0edfaf9a4749e900f
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-19 01:57:36 -0300

    Added the main function to generate the password

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ let sizePassword      = document.querySelector("#valor");               // Acess
 let password          = document.querySelector("#password");            // Acesso ao elemento password dentro do HTML
 let containerPassword = document.querySelector("#container-password");  // Acesso ao elemento container-password dentro do HTML
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&" // Todos os caracteres que podem ser usados ao gerar uma senha
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&" // Todos os caracteres que podem ser usados ao gerar uma senha
 let novaSenha = "";
 
-sizePassword.innerHTML = sliderElement.value;
-sliderElement.oninput = function(){
+sizePassword.innerHTML = sliderElement.value; // set the password size equal the actual slider element value
+
+sliderElement.oninput = function(){     // update the password size after move the slider
     sizePassword.innerHTML = this.value;
+//console.log(sizePassword.innerHTML)
 }
 
-    
+function generatePassword(){           /* add chars of the charset into the var "pass" based on the slideElement value and
+                                          the charset length, after this remove the hide class from the container password and make the var "password" equal "pass" */
+let pass = ""
+
+for(let i = 0, n= charset.length;i<sliderElement.value;++i ){
+   pass += charset.charAt(Math.floor(Math.random()* n))
+   
+ }
+//console.log(pass)
+containerPassword.classList.remove("hide");
+password.innerHTML = pass;
+}
