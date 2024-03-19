a6a11c8c299d3ca205f984b6a0c27ead9a061e55
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-18 23:34:18 -0300

    Added the variables fot password and the container-password and comments

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+let sliderElement     = document.querySelector("#slider");              // Acesso ao elemento slider dentro do HTML
+let buttonElement     = document.querySelector("#button");              // Acesso ao elemento button dentro do HTML
 
-let sizePassword = document.querySelector("#valor");
+let sizePassword      = document.querySelector("#valor");               // Acesso ao elemento valor dentro do HTML
+let password          = document.querySelector("#password");            // Acessp ao elemento password dentro do HTML
+let containerPassword = document.querySelector("#container-password");  // Acesso ao elemento container-password dentro do HTML
+
+
+
